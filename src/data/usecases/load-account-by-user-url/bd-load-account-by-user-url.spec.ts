c2b5cf2a446ca54b0af6db672202ea34a71b349a
Rafael Santos <rafael_santos.s1@hotmail.com>
2025-07-03 23:35:03 -0300

    test: Desenvolvimento do load-account-by-user-url para validar se a url do usuario é unica no BD.

---
+++ src/data/usecases/load-account-by-user-url/bd-load-account-by-user-url.spec.ts
@@ -0,0 +1,70 @@
+import { DbLoadAccountByUserUrl } from './bd-load-account-by-user-url'
+import type {
+  AccountModel,
+  LoadAccountByUserUrlRepository
+} from './bd-load-account-by-user-url-protocols'
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'valid_id',
+  name: 'valid_name',
+  email: 'valid_email@mail.com',
+  password: 'hashed_password',
+  role: 'valid_role',
+  userUrl: 'valid_userUrl'
+})
+
+const makeLoadAccountByUserUrlRepository = (): LoadAccountByUserUrlRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByUserUrlRepository {
+    async loadByUrl (userUrl: string): Promise<AccountModel> {
+      return await new Promise(resolve => {
+        resolve(makeFakeAccount())
+      })
+    }
+  }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+interface SutTypes {
+  sut: DbLoadAccountByUserUrl
+  loadAccountByUserUrlRepositoryStub: LoadAccountByUserUrlRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByUserUrlRepositoryStub = makeLoadAccountByUserUrlRepository()
+  const sut = new DbLoadAccountByUserUrl(loadAccountByUserUrlRepositoryStub)
+  return {
+    sut,
+    loadAccountByUserUrlRepositoryStub
+  }
+}
+
+describe('DbAddAccount UseCase', () => {
+  test('Should call LoadAccountByUserUrlRepository with correct values', async () => {
+    const { sut, loadAccountByUserUrlRepositoryStub } = makeSut()
+    const loadByUserUrlSpy = jest.spyOn(
+      loadAccountByUserUrlRepositoryStub,
+      'loadByUrl'
+    )
+    await sut.load('any_userUrl')
+    expect(loadByUserUrlSpy).toHaveBeenCalledWith('any_userUrl')
+  })
+
+  test('Should return null if LoadAccountByUserUrlRepository returns null', async () => {
+    const { sut, loadAccountByUserUrlRepositoryStub } = makeSut()
+    jest
+      .spyOn(loadAccountByUserUrlRepositoryStub, 'loadByUrl')
+      .mockReturnValueOnce(
+        new Promise(resolve => {
+          resolve(null)
+        })
+      )
+    const account = await sut.load('any_userUrl')
+    expect(account).toBeNull()
+  })
+
+  test('Should return an account on success', async () => {
+    const { sut } = makeSut()
+    const account = await sut.load('any_userUrl')
+    expect(account).toEqual('valid_userUrl')
+  })
+})
