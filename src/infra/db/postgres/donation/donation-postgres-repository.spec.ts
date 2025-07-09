3a4a0829860209ee2e893b76f4c05ac2c351d152
Rafael Santos <rafael_santos.s1@hotmail.com>
2025-07-09 13:05:59 -0300

    test: Implementação do Donation-Postgres-Repository, metodo AddDonation.

---
+++ src/infra/db/postgres/donation/donation-postgres-repository.spec.ts
@@ -0,0 +1,45 @@
+import { AppDataSource } from '@/infra/db/postgres/data-source/data-source'
+import { DonationPostgresRepository } from './donation-postgres-repository'
+import type { Repository } from 'typeorm'
+import Donation from '../entities/Donation'
+
+let accountRepository: Repository<Donation>
+
+describe('Account PostgreSQL Repository', () => {
+  beforeAll(async () => {
+    await AppDataSource.initialize()
+    accountRepository = AppDataSource.getRepository(Donation)
+  })
+
+  afterAll(async () => {
+    await AppDataSource.destroy()
+  })
+
+  beforeEach(async () => {
+    await accountRepository.delete({})
+  })
+
+  const makeSut = (): DonationPostgresRepository => {
+    return new DonationPostgresRepository()
+  }
+
+  describe('add()', () => {
+    test('Should return an account on add success', async () => {
+      const sut = makeSut()
+      const account = await sut.add({
+        name: 'any_name',
+        message: 'any_message',
+        creatorId: 'any_creatorId',
+        slug: 'any_slug',
+        price: 'any_price'
+      })
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.message).toBe('any_message')
+      expect(account.creatorId).toBe('any_creatorId')
+      expect(account.slug).toBe('any_slug')
+      expect(account.price).toBe('any_price')
+    })
+  })
+})
