3e032fbd08481b2acea05a2855b5d380b7a37220
Rafasantt <rafael_santos.s1@hotmail.com>
2025-08-24 10:39:02 -0300

    feat: Composição das objetos.

--- src/main/factories/usecases/account/update-donation-webHook/db-update-donation-webHook-factory.ts
+++ src/main/factories/usecases/account/update-donation-webHook/db-update-donation-webHook-factory.ts
@@ -1,10 +1,13 @@
 import type { UpdateDonationStatus } from '@/domain/usecases/update-donation-status'
 import { DbAUpdateDonationStatus } from '@/data/usecases/update-donation-status/db-update-donation-status'
 import { GetStripeDadaPaymentRepository } from '@/infra/gateways/stripe/webHooks/get-data-payment-stripe-webHooks'
+import { DonationPostgresRepository } from '@/infra/db/postgres/donation/donation-postgres-repository'
 
 export const makeDbUpdateStatusDonation = (): UpdateDonationStatus => {
   const getDataEventsRepository = new GetStripeDadaPaymentRepository()
+  const donationPostgresRepository = new DonationPostgresRepository()
   return new DbAUpdateDonationStatus(
-    getDataEventsRepository
+    getDataEventsRepository,
+    donationPostgresRepository
   )
 }
