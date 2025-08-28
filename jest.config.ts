e3bdb1639ff5e009bedda01324b446ca21e0e7b7
Rafasantt <rafael_santos.s1@hotmail.com>
2025-08-27 23:30:30 -0300

    chore: Ajustes na configuração do jest para não considerar alguns arquivo para gerar a cobertura de testes.

--- jest.config.ts
+++ jest.config.ts
@@ -4,9 +4,9 @@ const config: Config = {
 
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
     '!<rootDir>/src/main/**',
-    '!<rootDir>/src/data/usecases/add-account/bd-add-account-protocols.ts',
-    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/**',
     '!<rootDir>/src/infra/db/postgres/migrations/**',
     '!<rootDir>/src/presentation/protocols/index.ts'
   ],
